f7baaef8b2d929dd0dc489bda2f9e47180a05576
NirakChoun <nirakchoun@gmail.com>
2025-03-13 16:08:39 -0500

    Update link element to Link and NavLink

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
   return (
@@ -20,12 +21,12 @@ const JobListing = ({ job }) => {
             <i className="fa-solid fa-location-dot text-lg"></i>
             {job.location}
           </div>
-          <a
-            href="job.html"
+          <Link
+            to={`/jobs/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
