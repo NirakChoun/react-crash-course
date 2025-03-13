beab1b639791d0b2f8679abf72b35466b62884fb
NirakChoun <nirakchoun@gmail.com>
2025-03-13 15:46:12 -0500

    Clean nav link hrefs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
@@ -10,11 +11,7 @@ const Navbar = () => {
               className="flex flex-shrink-0 items-center mr-4"
               href="/index.html"
             >
-              <img
-                className="h-10 w-auto"
-                src="images/logo.png"
-                alt="React Jobs"
-              />
+              <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
               </span>
@@ -22,19 +19,19 @@ const Navbar = () => {
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
-                  href="/index.html"
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
                 </a>
                 <a
-                  href="/jobs.html"
+                  href="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Jobs
                 </a>
                 <a
-                  href="/add-job.html"
+                  href="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Job
